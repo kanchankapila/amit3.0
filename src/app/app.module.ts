7b471d93c5cc82d50509cc7addbc5b0027a03188
kanchankapila <127405216+kanchankapila@users.noreply.github.com>
2025-07-29 19:24:44 +0530

    feat: add caching interceptor and lazy image directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { CustomPreloadingStrategy } from './custom-preloading-strategy';
-import { AppComponent } from './app.component';
-import { SpinnerComponent } from './shared/spinner/spinner.component';
-import { ContentAnimateDirective } from './shared/directives/content-animate.directive';
-import { SharedModule } from './shared/shared.module';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { CustomPreloadingStrategy } from "./custom-preloading-strategy";
+import { AppComponent } from "./app.component";
+import { SpinnerComponent } from "./shared/spinner/spinner.component";
+import { ContentAnimateDirective } from "./shared/directives/content-animate.directive";
+import { SharedModule } from "./shared/shared.module";
+import { CacheInterceptor } from "./cache.interceptor";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SpinnerComponent,
-    ContentAnimateDirective
-    
-  ],
+  declarations: [AppComponent, SpinnerComponent, ContentAnimateDirective],
   imports: [
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     SharedModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
+    ServiceWorkerModule.register("ngsw-worker.js", {
       enabled: environment.production,
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: "registerWhenStable:30000",
     }),
   ],
   providers: [
-    { provide: Window, useValue: window },CustomPreloadingStrategy
+    { provide: Window, useValue: window },
+    CustomPreloadingStrategy,
+    { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
